6c24a052bf92e7f1c98405a223921b92f073f122
ivanmendozav <ivanmendozav@yahoo.com>
2022-12-24 14:55:16 -0500

    test

--- acfhc.py
+++ acfhc.py
@@ -1,1 +1,7 @@
-
+import utm
+import numpy as np
+import matplotlib.pyplot as plt
+v = utm.from_latlon(np.array([51.2, 51.3]),np.array([7.4, 7.5]))
+print(v)
+plt.plot(np.array([51.2, 51.3]),np.array([7.4, 7.5]))
+plt.show()
