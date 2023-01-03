8c1892274b8e0984c112f9109b5c758695f9babc
ivanmendozav <ivanmendozav@yahoo.com>
2023-01-03 11:54:55 -0500

    agregado el notebook

--- acfhc.py
+++ acfhc.py
@@ -1,7 +1,38 @@
-import utm
+#add libraries
+import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-v = utm.from_latlon(np.array([51.2, 51.3]),np.array([7.4, 7.5]))
-print(v)
-plt.plot(np.array([51.2, 51.3]),np.array([7.4, 7.5]))
-plt.show()
+import utm
+import time as time
+start = time.time()
+#add data
+datos = pd.read_csv("datasets/Viajes MEX-TOL.csv")
+#convert to utm
+latitudes = np.array(datos["latitud"])
+longitudes = np.array(datos["longitud"])
+utms = utm.from_latlon(latitudes,longitudes,14,"Q") #14Q for Mexico
+datos_f = datos.copy()
+datos_f["utm_x"] = utms[0]
+datos_f["utm_y"] = utms[1]
+datos_f = datos_f.drop(["l","latitud","longitud","fecha","hora"],axis=1)
+datos_f = datos_f.rename(columns={"altitud": "height"})
+#lag point coordinates
+datos_l = datos_f.copy()
+datos_l["next_utm_x"] = datos_f["utm_x"].shift(-1)
+datos_l["next_utm_y"] = datos_f["utm_y"].shift(-1)
+datos_l["next_height"] = datos_f["height"].shift(-1)
+#calculate distances and slopes
+datos_l["distance"] = np.sqrt((datos_l["utm_x"]-datos_l["next_utm_x"])**2 + (datos_l["utm_y"]-datos_l["next_utm_y"])**2)
+datos_l["slope"] = (datos_l["next_height"]-datos_l["height"])/datos_l["distance"] 
+datos_l["sum_distance"] = np.cumsum(datos_l["distance"])
+plt.plot(datos_l["sum_distance"],datos_l["height"])
+plt.show()    
+print(datos_l)
+stop = time.time()
+print(stop-start,"ms")
+
+def clustering(datos,tol):
+    clusters = np.ones(len(datos["height"]))
+    start=1
+    minpts = 2
+    
--- readme.txt
+++ readme.txt
@@ -1,5 +1,7 @@
-Para instalar extensión "matplotlib" desde línea de comandos (Cmd):
+Para instalar extensiones necesarias desde línea de comandos (Cmd):
+ivanm>py -m pip install numpy
 ivanm>py -m pip install matplotlib
+ivanm>py -m pip install pandas
 
-Para instalar extensión de github "utm" desde línea de comandos (Cmd):
+Para instalar extensión de github "utm", requerida para conversión de coordenadas, desde línea de comandos (Cmd):
 py -m pip install git+https://github.com/Turbo87/utm.git
